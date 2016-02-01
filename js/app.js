6cc512f643c60236a6c238a241fca87f614aff93
Peter M. Reynolds <txpmr1@gmail.com>
2016-02-01 14:38:02 -0600

    feat: use location objects, move input handling to player.update, add object widths

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 // Enemies our player must avoid
-var Enemy = function(point) {
+var Enemy = function(location) {
     // Variables applied to each of our instances go here,
-    this.x = point.x;
-    this.y = point.y;
+    this.x = location.x;
+    this.y = location.y;
     this.sprite = 'images/enemy-bug.png';
 };
+
+Enemy.prototype.width = 96;
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -23,28 +25,21 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(x, y) {
+var Player = function(location) {
     // Variables applied to each of our instances go here,
-    this.x = x;
-    this.y = y;
+    this.x = location.x;
+    this.y = location.y;
     this.sprite = 'images/char-boy.png';
 };
 
-// HANDLE INPUT
-Player.prototype.handleInput = function(key) {
-	if (key == 'up' & this.y >= 83) {
-		this.y -= 83;
-	} else if (key == 'down' & this.y <= 332) {
-		this.y += 83;
-	} else if (key == 'left' & this.x >= 101) {
-		this.x -= 101;
-	} else if (key == 'right' & this.x <= 303) {
-		this.x += 101;
-	}
-};
+Player.prototype.width = 66;
 
 // Update the player's position, required method for game
-Player.prototype.update = function() {
+Player.prototype.update = function(key) {
+	if      (key == 'up'    & this.y >=  83) { this.y -=  83; }
+	else if (key == 'down'  & this.y <= 332) { this.y +=  83; }
+	else if (key == 'left'  & this.x >= 101) { this.x -= 101; }
+	else if (key == 'right' & this.x <= 303) { this.x += 101; }
 };
 
 // Draw the player on the screen, required method for game
@@ -52,7 +47,7 @@ Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-genPoint = function() {
+var randomLocation = function() {
 	var x = (Math.floor((Math.random() * 5000) + 1)) *  -1;
 	var y = (Math.floor((Math.random() *    3) + 1)) *  83;
 	return {'x': x, 'y': y};
@@ -63,9 +58,16 @@ genPoint = function() {
 // Place the player object in a variable called player
 var allEnemies = [];
 for (var i=0; i<30; i++) {
-	allEnemies.push(new Enemy(genPoint()));
-}
-var player = new Player(202, 415);
+	allEnemies.push(new Enemy(randomLocation()));
+};
+var player = new Player({'x': 202, 'y': 415});
+
+// Decide what to do when a key is pressed
+var handleInput = function(key) {
+	if (['up', 'down', 'left', 'right'].indexOf(key) != -1) {
+		player.update(key);
+	}
+};
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -76,6 +78,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
-    player.handleInput(allowedKeys[e.keyCode]);
+    handleInput(allowedKeys[e.keyCode]);
 });
