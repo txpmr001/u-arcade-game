918617661d11adfdbde61aa02a7475929c85caf7
Peter M. Reynolds <txpmr1@gmail.com>
2016-02-23 08:49:44 -0600

    style: use uppercase for constant variables, use space for assignments and comparisons

--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
-// use semantic variables to make code simpler and easier to understand
-var canvasWidth  = 505;
-var canvasHeight = 606;
-var numCols      =   5;
-var numRows      =   6;
-var colWidth     = 101;
-var rowHeight    =  83;
-var col = ['dummy'];     // use col & row arrays to find canvas x, y values by col, row indexes
-var row = ['dummy'];     // dummy values make col and row indexes 1-based
-for (var x=0; x<numCols; x++) {col.push(x*colWidth);}
-for (var y=0; y<numRows; y++) {row.push(y*rowHeight);}
+// use semantic constant variables to make code simpler and easier to understand
+var CANVAS_WIDTH  = 505;
+var CANVAS_HEIGHT = 606;
+var NUM_COLS      =   5;
+var NUM_ROWS      =   6;
+var COL_WIDTH     = 101;
+var ROW_HEIGHT    =  83;
+var COL_X = ['dummy'];     // use col & row arrays to find canvas x, y values by col, row indexes
+var ROW_Y = ['dummy'];     // dummy values make col and row indexes 1-based
+for (var x = 0; x < NUM_COLS; x++) {COL_X.push(x * COL_WIDTH);}
+for (var y = 0; y < NUM_ROWS; y++) {ROW_Y.push(y * ROW_HEIGHT);}
 
 // check for a collision between a single object and an array of objects
 var collides = function(obj1, array2) {
-	var obj1x = obj1.x + ((colWidth - obj1.width) / 2);         // x of object1 left edge
+	var obj1x = obj1.x + ((COL_WIDTH - obj1.width) / 2);       // x of object1 left edge
 	var arrayLength = array2.length;
 	var collision   = 0;
-	for (var i=0; i<arrayLength; i++) {
+	for (var i = 0; i < arrayLength; i++) {
 		var obj2 = array2[i];
 		if (obj1 === obj2) { continue; }                        // can't collide with yourself
-		var obj2x = obj2.x + ((colWidth - obj2.width) / 2);     // x of object2 left edge
+		var obj2x = obj2.x + ((COL_WIDTH - obj2.width) / 2);    // x of object2 left edge
 		// use bounding box collision detection
 		collision = collision || obj1.y == obj2.y && obj1x < (obj2x+obj2.width) && (obj1x+obj1.width) > obj2x;
 	}
@@ -36,8 +36,8 @@ var Enemy = function() {
 // generate a left of screen location without overlap
 Enemy.prototype.randomize = function() {
 	do {
-		this.x = (Math.floor((Math.random() * 1000) + colWidth)) * -1;  // x = -colWidth to -(999+colWidth)
-		this.y = (Math.floor((Math.random() *    3) + 1)) * rowHeight;  // y = row 1 to 3
+		this.x = (Math.floor((Math.random() * 1000) + COL_WIDTH)) * -1;  // x = -COL_WIDTH to -(999+COL_WIDTH)
+		this.y = (Math.floor((Math.random() *    3) + 1)) * ROW_HEIGHT;  // y = row 1 to 3
 	} while (collides(this, allEnemies));
 };
 
@@ -46,12 +46,12 @@ Enemy.prototype.randomize = function() {
 // enemies move left to right
 Enemy.prototype.update = function(dt) {
     // movements are multiplied by dt to ensure consistent game speed across all computers
-	this.x += dt * (((this.y/rowHeight)*100));
+	this.x += dt * (((this.y / ROW_HEIGHT) * 100));
 	if (collides(this, [player])) {                       // if enemy collides with player
-		player.score = Math.max(0, player.score-100);     //   -100 points	
+		player.score = Math.max(0, player.score - 100);     //   -100 points	
 		player.startLocation();                           //   place player at starting location
 	}
-	if (this.x > canvasWidth) {     // if enemy moves off right of screen
+	if (this.x > CANVAS_WIDTH) {     // if enemy moves off right of screen
 		this.randomize();           //   generate a random left of screen location
 	}
 };
@@ -71,8 +71,8 @@ var Gem = function() {
 
 // generate a random location, delay, and duration
 Gem.prototype.randomize = function() {
-	this.x = col[Math.floor((Math.random() * 5) + 1)];              // col 1 to 5
-	this.y = row[Math.floor((Math.random() * 3) + 2)];              // row 2 to 4
+	this.x = COL_X[Math.floor((Math.random() * 5) + 1)];            // col 1 to 5
+	this.y = ROW_Y[Math.floor((Math.random() * 3) + 2)];            // row 2 to 4
 	this.displayDelay    = Math.floor((Math.random() * 6) + 3);     // delay   for 3 to 8 seconds
 	this.displayDuration = Math.floor((Math.random() * 3) + 3);     // display for 3 to 5 seconds
 };
@@ -121,8 +121,8 @@ var Player = function() {
 
 // place player at starting location
 Player.prototype.startLocation = function() {
-	this.x = col[3];
-	this.y = row[6];
+	this.x = COL_X[3];
+	this.y = ROW_Y[6];
 };
 
 // update the player position and game situation
@@ -130,7 +130,7 @@ Player.prototype.update = function(key) {
 	if (this.pause && key == 'space') {     // new game
 		this.showInstructions = 0;          // only display instructions once
 		ctx.globalAlpha = 1;                // un-dim game
-		ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+		ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 		if (allEnemies.length > 5) {        // reduce enemies to 5
 			allEnemies.splice(5, allEnemies.length - 5);
 		}
@@ -141,9 +141,9 @@ Player.prototype.update = function(key) {
 	}
 	else if (this.pause && key !== 'space') {      // ignore all but spacebar if paused
 	}
-	else if (key == 'up' && this.y > row[1]) {     // move up
-		this.y -= rowHeight;                       // decrement y
-		if (this.y == row[1]) {                    // if crossed road
+	else if (key == 'up' && this.y > ROW_Y[1]) {   // move up
+		this.y -= ROW_HEIGHT;                      // decrement y
+		if (this.y == ROW_Y[1]) {                  // if crossed road
 			this.score += 500;                     //   +500 points
 			for (var i=0; i<2; i++) {              //   add 2 enemies
 				var enemy = new Enemy();
@@ -151,9 +151,9 @@ Player.prototype.update = function(key) {
 			this.startLocation();                  // place player at starting location
 		}
 	}
-	else if (key == 'down'  && this.y < row[6]) { this.y += rowHeight; }     // move down
-	else if (key == 'left'  && this.x > col[1]) { this.x -= colWidth;  }     // move left
-	else if (key == 'right' && this.x < col[5]) { this.x += colWidth;  }     // move right	
+	else if (key == 'down'  && this.y < ROW_Y[6]) { this.y += ROW_HEIGHT; }     // move down
+	else if (key == 'left'  && this.x > COL_X[1]) { this.x -= COL_WIDTH;  }     // move left
+	else if (key == 'right' && this.x < COL_X[5]) { this.x += COL_WIDTH;  }     // move right	
 
 	if (!this.pause) {
 		var currentTime = Date.now();                      // current time
@@ -170,7 +170,7 @@ Player.prototype.update = function(key) {
 // draw the player on the screen
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y-10);
-	ctx.clearRect(0, 0, canvasWidth, 50);     // display score & time
+	ctx.clearRect(0, 0, CANVAS_WIDTH, 50);     // display score & time
 	ctx.font      = '30px sans-serif';
 	ctx.fillStyle = 'black';
 	ctx.textAlign = 'left';
@@ -180,28 +180,28 @@ Player.prototype.render = function() {
 
 	if (player.pause) {	                      // if player paused
 		ctx.globalAlpha = 0.5;                //   dim screen
-		ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+		ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
  		ctx.font      = '30px sans-serif';
 		ctx.fillStyle = 'white';
 		ctx.textAlign = 'center';
 		if (player.showInstructions) {        //   show instructions for 1st game
-			ctx.fillText('Use the arrow keys to move', canvasWidth/2, 160);
-			ctx.fillText('your character across the road', canvasWidth/2, 195);
-			ctx.fillText('to the water. (+500 pts)', canvasWidth/2, 230);
-			ctx.fillText('Avoid enemy bugs. (-100 pts)', canvasWidth/2, 280);
-			ctx.fillText('Capture blue gems', canvasWidth/2, 330);
-			ctx.fillText('for bonus points. (+200 pts)', canvasWidth/2, 365);
-			ctx.fillText('Press spacebar to start.', canvasWidth/2, 440);
+			ctx.fillText('Use the arrow keys to move', CANVAS_WIDTH/2, 160);
+			ctx.fillText('your character across the road', CANVAS_WIDTH/2, 195);
+			ctx.fillText('to the water. (+500 pts)', CANVAS_WIDTH/2, 230);
+			ctx.fillText('Avoid enemy bugs. (-100 pts)', CANVAS_WIDTH/2, 280);
+			ctx.fillText('Capture blue gems', CANVAS_WIDTH/2, 330);
+			ctx.fillText('for bonus points. (+200 pts)', CANVAS_WIDTH/2, 365);
+			ctx.fillText('Press spacebar to start.', CANVAS_WIDTH/2, 440);
 		} else {                              //   show play again instructions
-			ctx.fillText('Press spacebar to play again.', canvasWidth/2, 440);
+			ctx.fillText('Press spacebar to play again.', CANVAS_WIDTH/2, 440);
 		}
 	}
 };
 
 //----------------------------------------------------------
 // instantiate enemy, gem, and player objects
-var allEnemies = [];          // enemy objects are contained in array allEnemies
-for (var i=0; i<5; i++) {     // start with 5 enemies
+var allEnemies = [];              // enemy objects are contained in array allEnemies
+for (var i = 0; i < 5; i++) {     // start with 5 enemies
 	var enemy = new Enemy();
 }
 var gem = new Gem();
